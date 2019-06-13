fb67299931731208b0cfaea859e12c702a3bbcc9
Kovbasiuk Oleksii <kovbasiuk.oleksii@gmail.com>
2019-06-13 15:45:56 +0300

    commit

--- company/company.js
+++ company/company.js
@@ -22,3 +22,40 @@ $(".btn-with-icon").on("click", function() {
         $(".wave-anim").removeClass('visible');
     });
 });
+
+const search = document.querySelector('.search');
+const body = document.querySelector('body');
+
+search.addEventListener('click', function (e) {
+    e.stopPropagation();
+    this.classList.add('search-active');
+});
+
+body.addEventListener('click', function () {
+    search.classList.remove('search-active');
+});
+
+var clock = document.getElementById('clock');
+
+function hexoClock() {
+    var time = new Date();
+    var h = time.getHours().toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
+    
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }
+    
+    var clockString = h + ':' + m + ':' + s;
+    clock.textContent = clockString;
+}
+
+hexoClock();
+setInterval(hexoClock, 1000);
