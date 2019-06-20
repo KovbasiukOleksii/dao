8725d90e94057ba30a9c21242ec42cc1313a08dd
Kovbasiuk Oleksii <kovbasiuk.oleksii@gmail.com>
2019-06-20 15:18:35 +0300

    commit

---
+++ user_info/user_info.js
@@ -0,0 +1,78 @@
+(function() {
+    var selectors = {
+        nav: '[data-features-nav]',
+        tabs: '[data-features-tabs]',
+        active: '.__active'
+    }
+    var classes = {
+        active: '__active'
+    }
+    $('a', selectors.nav).on('click', function() {
+        let $this = $(this)[0];
+        $(selectors.active, selectors.nav).removeClass(classes.active);
+        $($this).addClass(classes.active);
+        $('div', selectors.tabs).removeClass(classes.active);
+        $($this.hash, selectors.tabs).addClass(classes.active);
+        return false
+    });
+}());
+
+$(".btn-with-icon").on("click", function() {
+    $(".wave-anim").addClass('visible').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd", function() {
+        $(".wave-anim").removeClass('visible');
+    });
+});
+
+
+const search = document.querySelector('.search');
+const body = document.querySelector('body');
+
+search.addEventListener('click', function (e) {
+    e.stopPropagation();
+    this.classList.add('search-active');
+});
+
+body.addEventListener('click', function () {
+    search.classList.remove('search-active');
+});
+
+var clock = document.getElementById('clock');
+
+function hexoClock() {
+    var time = new Date();
+    var h = time.getHours().toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
+    
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }
+    
+    var clockString = h + ':' + m + ':' + s;
+    clock.textContent = clockString;
+}
+
+hexoClock();
+setInterval(hexoClock, 1000);
+
+$(document).on('click', '.lang a', function(){
+    $(this).addClass('active_lang').siblings().removeClass('active_lang')
+});
+
+
+$(document).ready(function(){
+    $("#mobileMenu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+new WOW().init();
